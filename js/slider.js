3e53d1075234fe5a47b1ac86a72b64b464296e1c
Electruuu <60323922+Electruuu@users.noreply.github.com>
2024-05-24 18:45:11 +0200

    collab na PSI
    Co-Authored-By: Ven0m474 <70232322+Ven0m474@users.noreply.github.com>

---
+++ js/slider.js
@@ -0,0 +1,33 @@
+let curImage = 0
+const sliderRef = document.querySelector("#sliderImg")
+
+const imgs = [
+    "gallery1.jpg",
+    "gallery2.jpg",
+    "gallery3.jpg",
+    "gallery4.jpg",
+    "gallery5.jpg",
+    "gallery6.jpg",
+    "gallery7.jpg",
+    "gallery8.jpg",
+    "gallery9.jpg",
+    "gallery10.jpg"
+]
+function decImg() {
+    curImage--
+    if (curImage<0) {
+        curImage = imgs.length-1;
+    }
+    sliderRef.src = `img/${imgs[curImage]}`
+}
+function incImg() {
+    curImage++
+    if (curImage>=imgs.length) {
+        curImage = 0;
+    }
+    sliderRef.src = `img/${imgs[curImage]}`
+    console.log(`img/${imgs[curImage]}`)
+}
+const sliderBtns = document.querySelectorAll("#slider button")
+sliderBtns[0].addEventListener("click", decImg)
+sliderBtns[1].addEventListener("click", incImg)
